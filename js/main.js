2bca9b8b30a2836ba7a261cd7f0bb5ba97355dc8
banyurachman <banyurachman95@gmail.com>
2019-02-19 20:33:22 +0700

    Our work section progress.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $( document ).ready(function() {
     $('.parallax').parallax();
     $('.sidenav').sidenav();
+    $('.carousel').carousel({
+      dist: 0,
+      padding: 10,
+      numVisible: 4
+    });
+    $('#buttonLeft').click(function(){
+      $(".slider").scrollLeft(1000);
+    });
+    $('#buttonRight').click(function(){
+      $(".slider").scrollRight(1000);
+    });
 });
