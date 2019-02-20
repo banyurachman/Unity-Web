d52b98888f3cecff60422fe1c494413c8f4e61ee
banyurachman <banyurachman95@gmail.com>
2019-02-20 12:18:28 +0700

    MVP done!

--- js/main.js
+++ js/main.js
@@ -6,10 +6,77 @@ $( document ).ready(function() {
       padding: 10,
       numVisible: 4
     });
-    $('#buttonLeft').click(function(){
-      $(".slider").scrollLeft(1000);
+
+    var headerHeight = $('#homeSection').height();
+
+    if($(window).scrollTop() > headerHeight-1){
+      $('#mainMenuFixed').show();
+    }else{
+      $('#mainMenuFixed').hide();
+    }
+
+    $(window).scroll(function (event) {
+        if($(window).scrollTop() > headerHeight-1){
+          $('#mainMenuFixed').show();
+        }else{
+          $('#mainMenuFixed').hide();
+        }
+        // Do something
     });
-    $('#buttonRight').click(function(){
-      $(".slider").scrollRight(1000);
+
+    function checkActiveSection(){
+      var sections = ["homeSection","aboutUsSection","whatWeDoSection","meetTheTeamSection","ourWorkSection","getInTouchSection"];
+
+      sections.forEach(function(section){
+          if($(this).scrollTop()>=$('#'+section).position().top-20 && $(this).scrollTop()<($('#'+section).position().top+$('#'+section).height())){
+              if(!$("."+section+"Link").hasClass("active")){
+                  $("."+section+"Link").addClass("active");
+              }
+          }else{
+              if($("."+section+"Link").hasClass("active")){
+                  $("."+section+"Link").removeClass("active");
+              }
+          }
+      });
+
+      if($(window).scrollTop() + $(window).height() == $(document).height()) {
+         $(".getInTouchSectionLink").addClass("active");
+         $(".ourWorkSectionLink").removeClass("active");
+       }
+
+    }
+
+    checkActiveSection()
+
+    $(document).on('scroll', function() {
+        checkActiveSection();
     });
+
+    scrollIt = function(){
+      $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+        var position = $($(this).attr("href")).offset().top;
+        $("body, html").animate({
+          scrollTop: position
+        });
+      });
+    }
+
+    scrollIt();
+
+    ScrollReveal().reveal('#homeSection a img', {delay:400});
+    ScrollReveal().reveal('#headerMainTitle', {delay:800});
+    ScrollReveal().reveal('#mainMenu', {delay:1000});
+    ScrollReveal().reveal('.sectionTitle');
+    ScrollReveal().reveal('section h1', {delay:800});
+    ScrollReveal().reveal('section p', {delay:800});
+    ScrollReveal().reveal('section img', {delay:1200});
+    ScrollReveal().reveal('#aboutUsSection .sectionTitle', {delay:1000});
+    ScrollReveal().reveal('#aboutUsSection p', {delay:1200});
+    ScrollReveal().reveal('#whatWeDoSection h3', {delay:1400});
+    ScrollReveal().reveal('#meetTheTeamSection .name', {delay:1100});
+    ScrollReveal().reveal('#meetTheTeamSection .title', {delay:1200});
+    ScrollReveal().reveal('#meetTheTeamSection .socmed', {delay:1500});
+    ScrollReveal().reveal('#ourWorkSection .slider', {delay:1200});
+
 });
